8788099a0e6f0b14a92d6b246f0fbbc379bd27ba
DvDCherepashka <Cherepashkabox@gmail.com>
2020-10-08 18:55:24 +1000

    m

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 print("Kono Dio Da 🤡�����🤡�����🤡�����🤡�����🤡")
-print("�������������������������Marunara�������������������������")
+print("Marunara")
 a = 10
 a += 1
 print(a + 20 / 2 * 3 ** 2)
+
+b = 20 + 10 / 4 * 40
+print(b)
+
+d = "a"
+name = "это мой первый код"
+print(d + name)
